2fa5a9d6340705d2cd291439b77ee0d092b1fe2b
jay <wjay.inclino@gmail.com>
2025-06-22 10:23:05 +0800

    update title to Gymly PH

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -19,7 +19,7 @@ export default defineNuxtConfig({
     app: {
         baseURL: '/gymly/',
         head: {
-            title: 'Gymly',
+            title: 'Gymly PH',
             meta: [
                 { charset: 'utf-8' },
                 { name: 'viewport', content: 'width=device-width, initial-scale=1' },
