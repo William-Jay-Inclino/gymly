a82f89fe4a0e944ea323ca0c171abdf3f5e27ffa
jay <wjay.inclino@gmail.com>
2025-06-22 10:03:17 +0800

    add homepage v3

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -19,7 +19,7 @@
                 </p>
                 <button
                     class="btn btn-lg bg-gradient-to-r from-primary to-blue-500 text-white border-0 shadow-lg hover:scale-105 hover:from-blue-600 hover:to-primary-focus transition-transform duration-150 flex items-center gap-2"
-                    @click="signUpWithGoogle"
+                    @click="login_with_google"
                 >
                     <svg aria-label="Google logo" width="20" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
                     <span class="font-semibold tracking-wide">Signup with Google</span>
@@ -159,10 +159,11 @@ import {
     CrownIcon
 } from 'lucide-vue-next';
 
-function signUpWithGoogle() {
-    // TODO: Integrate Google sign-in
-    alert('Google sign-up coming soon!');
+
+function login_with_google() {
+    window.location.href = `${API_URL}/auth/google/signup`
 }
+
 </script>
 
 <style scoped>
